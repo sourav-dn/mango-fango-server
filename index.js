958ff90baf4631761282a11a740a6cc699055cf9
sourav-dn <souravdn.dev@gmail.com>
2025-05-24 12:52:53 +0600

    add server client

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ app.get('/', (req, res) => {
 
 
 
-
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
 
 
 
